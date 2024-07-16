beaca96d48398b0b999ab6a9d6ba2504487493ed
dydev012 <dydev012@gmail.com>
2024-07-17 00:00:42 +0100

    prevent showing LaTex in article summaries

---
+++ filters.py
@@ -0,0 +1,20 @@
+import re
+
+def clean_mathjax(content):
+    # Regular expression to remove MathJax delimiters and content
+    mathjax_pattern = re.compile(r'(\$\$.*?\$\$|\$.*?\$)', re.DOTALL)
+    cleaned_content = re.sub(mathjax_pattern, '', content)
+    return cleaned_content
+
+def extract_first_p_tag_content(text):
+    # Regex pattern to match everything between the first pair of <p> tags
+    pattern = re.compile(r'<p>(.*?)</p>', re.DOTALL)
+    
+    # Finding the first match
+    match = re.search(pattern, text)
+    
+    if match:
+        return match.group(1)  # Return the content inside the first <p> tags
+    else:
+        return ""  # Return empty string if no match is found
+    
--- pelicanconf.py
+++ pelicanconf.py
@@ -26,12 +26,27 @@ SOCIAL = (
             ('github', 'https://github.com/dydev012'),
             ('envelope', 'mailto:dydev012@gmail.com'),)
 
-DEFAULT_PAGINATION = 5
+SUMMARY_MAX_LENGTH = 100
+SUMMARY_END_SUFFIX = '…'
+DEFAULT_PAGINATION = 2
 
 # https://github.com/pelican-plugins/render-math?tab=readme-ov-file#settings
-# MATH_JAX = {
-
-# }
+MATH_JAX = {
+'align': 'center',
+'auto_insert': True,
+'indent': '0em',
+'show_menu': True,
+'process_escapes': True,
+'MathJax_font': 'default',
+'latex_preview': 'Tex',
+'color': 'inherit',
+'linebreak_automatic': False,
+'tex_extensions': [],
+'responsive': False,
+'responsive_break': 768,
+'process_summary': False,
+'message_style': 'normal'
+}
 
 # enable search within theme
 TAG_SAVE_AS = ''
@@ -40,3 +55,19 @@ DIRECT_TEMPLATES = ('index', 'categories', 'archives', 'search', 'tipue_search')
 TIPUE_SEARCH_SAVE_AS = 'tipue_search.json'
 # Uncomment following line if you want doscument-relative URLs when developing
 # RELATIVE_URLS = True
+
+from filters import clean_mathjax, extract_first_p_tag_content
+def generate_summary(content):
+    # if in markup form
+    nojax_text = extract_first_p_tag_content(content)
+    nojax_text = ' '.join(clean_mathjax(nojax_text).split(' ')[:SUMMARY_MAX_LENGTH])
+    nojax_text = nojax_text.rstrip()
+    print(nojax_text)
+    print()
+
+    
+    return nojax_text
+
+JINJA_FILTERS = {
+    'generate_summary': generate_summary,
+}
